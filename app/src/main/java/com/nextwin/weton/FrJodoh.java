879593708b7cf131738304c2d18faa6169fd38b0
faozi07 <syaeful.faozi93@gmail.com>
2018-10-19 23:24:57 +0700

    weton terbaru

---
+++ app/src/main/java/com/nextwin/weton/FrJodoh.java
@@ -0,0 +1,108 @@
+package com.nextwin.weton;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import static com.nextwin.weton.Hasil.isJodoh;
+import static com.nextwin.weton.Hasil.jodoh;
+
+public class FrJodoh extends Fragment {
+    TextView tHasilJodoh;
+    AdView mAdView;
+    LinearLayout layLoading;
+    ScrollView scrollView;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fr_jodoh, container, false);
+        tHasilJodoh = v.findViewById(R.id.textHasilJodoh);
+        mAdView = v.findViewById(R.id.adView);
+        layLoading = v.findViewById(R.id.layLoading);
+        scrollView = v.findViewById(R.id.scrollView);
+        scrollView.setVisibility(View.GONE);
+        layLoading.setVisibility(View.VISIBLE);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+        mAdView.setAdListener(new AdListener() {
+
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+            }
+
+            @Override
+            public void onAdOpened() {
+                super.onAdOpened();
+            }
+
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int i) {
+                super.onAdFailedToLoad(i);
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                super.onAdLeftApplication();
+            }
+        });
+        if (isJodoh) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    tHasilJodoh.setText(jodoh);
+                    scrollView.setVisibility(View.VISIBLE);
+                    layLoading.setVisibility(View.GONE);
+                    isJodoh = false;
+                }
+            }, 2000);
+        } else {
+            tHasilJodoh.setText(jodoh);
+            scrollView.setVisibility(View.VISIBLE);
+            layLoading.setVisibility(View.GONE);
+        }
+        return v;
+    }
+    @Override
+    public void onPause() {
+        // This method should be called in the parent Activity's onPause() method.
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // This method should be called in the parent Activity's onResume() method.
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        // This method should be called in the parent Activity's onDestroy() method.
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
+    }
+}
